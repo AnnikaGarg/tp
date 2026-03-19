157e732f27cd4ec5f41e3f7328a8694bc1038d8a
Nishchay Sanghi <snishchay06@gmail.com>
2026-03-19 18:33:25 +0800

    Fixed minor ui issue

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -53,10 +53,10 @@ public class Ui {
         System.out.println("Here are the available commands:");
         System.out.println("  add AMOUNT DESCRIPTION  - Add a new expense");
         System.out.println("  list                    - List all expenses");
+        System.out.println("  total                   - Calculate the total amount spent");
         System.out.println("  delete INDEX            - Delete an expense by index");
         System.out.println("  help                    - Show this help menu");
         System.out.println("  exit                    - Exit the application");
-        System.out.println("  total                   - Calculate the total amount spent");
         System.out.println(LINE);
     }
 
