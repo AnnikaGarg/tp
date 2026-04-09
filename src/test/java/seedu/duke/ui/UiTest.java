2a30917100a65cdb1f3903e499fd2122549f66cb
Annika Garg <annikaajaygarg@gmail.com>
2026-04-09 20:41:24 +0800

    Remove unused import from UiTest

--- src/test/java/seedu/duke/ui/UiTest.java
+++ src/test/java/seedu/duke/ui/UiTest.java
@@ -1,6 +1,5 @@
 package seedu.duke.ui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import seedu.duke.model.Expense;
